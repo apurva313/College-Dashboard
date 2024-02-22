583bd76b00d5f09eb4ac041b13b9fe62dfd63eb6
apurva313 <apurvakumar313@gmail.com>
2024-02-22 11:58:46 +0530

    Update App.js and Transport.js

--- client/src/component/pages/Transport.js
+++ client/src/component/pages/Transport.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react'
 import axios from "axios";
-
+import {ToastContainer, toast} from 'react-toastify'
 const Transport = () => {
     const [name, setName] = useState('');
     const [job, setJob] = useState('');
     const [loading, setLoading] = useState(false);
     const [isError, setIsError] = useState(false);
-    const [data, setData] = useState('null');
-
+    const [data, setData] = useState(null);
     const sb = () => {
         setLoading(true);
         setIsError(false);
@@ -22,7 +21,13 @@ const Transport = () => {
             setLoading(false);
             setIsError(true);
         });
+
     }
+    const toastInfo = ()=> toast.info('information');
+    const toastSuccess = ()=> toast.success('success');
+    const toastWarn = ()=> toast.warn('warning');
+    const toastError = ()=> toast.error('error');
+
     return <>
         <div>
             <label>Name</label>
@@ -30,23 +35,24 @@ const Transport = () => {
             <label>Job</label>
             <input type='text' id='job' placeholder='enter job' value={job} onChange={e => setJob(e.target.value)} />
         </div>
-
-        {
-            isError && <p>Error try again</p>
-        }
+        {isError && <p>Error try again</p>}
         <button type='submit' className='btn btn-danger' onClick={sb} disabled={loading}>
-            {
-                loading ? 'loading.....' : 'submit'
-            }
+            {loading ? 'loading...' : 'submit'}
         </button>
-        {
-            data && <div>
-                <pre>{JSON.stringify(data, null)}</pre>
-                
-            </div>
-        }
+        {data && <div>
+            <pre>{JSON.stringify(data,null,2)}</pre>
+        </div>}
 
+        <div>
+            <button className='btn btn-dark' onClick={toastInfo}>INFO</button>
+            <button className='btn btn-dark' onClick={toastSuccess}>success</button>
+            <button className='btn btn-dark' onClick={toastWarn}>warning</button>
+            <button className='btn btn-dark' onClick={toastError}>error</button>
+
+            <ToastContainer position='bottom-right'autoClose={5000} closeButton/>
+        </div>
     </>
+
 }
 
 export default Transport
